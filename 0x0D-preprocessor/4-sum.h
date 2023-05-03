f28d59582e54dce73af18e5023283a1175a84a63
ZEYAD-8 <zeyadosama15@gmail.com>
2023-05-03 06:15:40 +0300

    Defer

--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,4 +0,0 @@
-#ifndef OBJECT_LIKE_MACRO_H
-#define OBJECT_LIKE_MACRO_H
-#define SUM(x, y) ((y) + (x))
-#endif /* OBJECT_LIKE_MACRO_H */
--- 0x12-singly_linked_lists/100-first.c
+++
@@ -1,9 +0,0 @@
-#include "lists.h"
-
-void print_before_main(void) __attribute__((constructor));
-
-void print_before_main(void)
-{
-printf("You're beat! and yet, you must allow,\n");
-printf("I bore my house upon my back!\n");
-}
