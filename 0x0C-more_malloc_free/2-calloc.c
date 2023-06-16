d3d9972907d16fcd62074032f6d1c339a9141e90
ZEYAD-8 <zeyadosama15@gmail.com>
2023-06-16 08:45:34 -0700

    2. _calloc

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include <stdio.h>
+/**
+ * *_calloc - creates an array of nmemb elements with size of "size" each
+ * @nmemb: the number of elements
+ * @size: size of each element
+ * Return: a pointer to the memory allocated.
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *p;
+	unsigned int i;
+
+	if (nmemb * size == 0)
+		return (NULL);
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb; i++)
+	{
+		p[i] = 0;
+	}
+	return (p);
+}
+
