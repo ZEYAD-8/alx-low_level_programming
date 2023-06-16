298ba5e87189c9abf243ea5f60a3f08a755d8e52
ZEYAD-8 <zeyadosama15@gmail.com>
2023-06-16 10:22:07 -0700

    2. _calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -19,7 +19,8 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	p = calloc(nmemb * size);
 	if (p == NULL)
 		return (NULL);
-	*p = 0;
+	for (i = 0; i < (nmemb * size); i++)
+		*(p + i) = 0;
 	return (p);
 }
 
