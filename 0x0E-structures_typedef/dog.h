2c4fca16646624716785b84f87d308e6d02640de
ZEYAD-8 <zeyadosama15@gmail.com>
2023-06-19 13:40:34 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog details structure
+ * @name: name
+ * @age: age
+ * @owner: owner
+ *
+ * description: none
+ */
+struct struct dog {
+char *name;
+float age;
+char *owner;
+};
+
+#endif
