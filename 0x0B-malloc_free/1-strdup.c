8a4f6671d7e4f20928fe8003957eaddae38b6abe
ZEYAD-8 <zeyadosama15@gmail.com>
2023-06-24 07:15:27 -0700

    1. The woman who has no imagination has no wings

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strdup - entry point
+ * @str: string
+ * Return: pointer
+*/
+char *_strdup(char *str)
+{
+	char *new;
+
+	if (str == NULL)
+		return (NULL);
+	new = malloc(stlen(str) + 1);
+	if (new == NULL)
+		return (NULL);
+	strcpy(new, str);
+	return (new);
+}
