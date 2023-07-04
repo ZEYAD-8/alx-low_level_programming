cb61ea0262879dfacfb483f21ea9069b8af01e2f
ZEYAD-8 <zeyadosama15@gmail.com>
2023-07-04 11:35:00 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "main.h"
@@ -11,37 +10,26 @@
 */
 char *str_concat(char *s1, char *s2)
 {
-	int i, s1_len, s2_len, is_s2_null, is_s1_null, s1_flag;
-	char *new_string;
+	size_t len1, len2;
+	char *result;
 
-	s1_len = s2_ len = is_s1_null = is_s2_null = 0;
 	if (s1 == NULL)
-		is_s1_null = 1;
-	else
-		s1_len = strlen(s1);
+		s1 = "";
 
 	if (s2 == NULL)
-		is_s2_null = 1;
-	else
-		s2_len = strlen(s2);
+		s2 = "";
 
-	new_string = malloc(sizeof(char) * (s1_len + s2_len));
-	if (new_string == NULL)
+	len1 = strlen(s1);
+	len2 = strlen(s2);
+
+	result = malloc(len1 + len2 + 1);
+
+	if (result == NULL)
 		return (NULL);
 
-	for (i = 0, len = s1_len, s1_flag = 1; i < len; i++)
-	{
-		if (len == 0)
-		{
-			s1_flag = 0;
-			len = s2_len;
-		}
+	memcpy(result, s1, len1);
+	memcpy(result + len1, s2, len2 + 1);
 
-		if (s1_flag)
-			new_string[i] = s1[i];
-		else
-			new_string[i] = s1[i + s1_len];
-	}
-	new_string[s1_len + s2_len + 1] = '\0';
-	return (new_string);
+	return (result);
 }
+
