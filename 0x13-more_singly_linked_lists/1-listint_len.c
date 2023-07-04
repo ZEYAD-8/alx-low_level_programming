d73eda8131103c93307091253516378e231b88d6
ZEYAD-8 <zeyadosama15@gmail.com>
2023-07-04 08:01:21 -0700

    1. List length

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,9 @@
 #include "lists.h"
-
+/**
+ * listint_len - counts the number of nodes in a list
+ * @h: the head of the list
+ * Return: the number of nodes
+*/
 size_t listint_len(const listint_t *h)
 {
 	const listint_t *tmp;
@@ -14,3 +18,4 @@ size_t listint_len(const listint_t *h)
 	}
 	return (nodes);
 }
+
