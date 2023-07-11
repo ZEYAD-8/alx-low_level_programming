7796e57b39d072155564e3e6fd878408c2cd93a9
ZEYAD-8 <zeyadosama15@gmail.com>
2023-07-11 05:59:02 +0300

    N

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -12,7 +12,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <elf.h>
-#include "helper_functions.c"
+
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
