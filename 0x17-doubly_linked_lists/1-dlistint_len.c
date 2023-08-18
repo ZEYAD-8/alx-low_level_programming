082833ecfcecc82d8a0e33ebdfdc2d87ad434d95
ZEYAD-8 <zeyadosama15@gmail.com>
2023-08-18 11:05:03 +0300

    List length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * dlistint_len - counts number of nodes in a doubly linked list
+ * @h: the head of the list
+ * Return: the number of nodes in the list
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	dlistint_t *tmp;
+
+	if (h == NULL)
+		return (0);
+	tmp = h->next;
+	while (tmp != NULL)
+	{
+		count++;
+		tmp = tmp->next;
+	}
+	return (++count);
+}
