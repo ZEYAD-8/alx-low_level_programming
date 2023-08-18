eed5e149b5c856f809f491c43910072d7fa24881
ZEYAD-8 <zeyadosama15@gmail.com>
2023-08-18 13:06:28 +0300

    Task 6: Sum list

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+int sum_dlistint(dlistint_t *head)
+{
+    dlistint_t *tmp;
+    int sum = 0;
+
+    if (head == NULL)
+    {
+        return (0);
+    }
+    tmp = head;
+    while (tmp != NULL)
+    {
+        sum += tmp->n;
+        tmp = tmp->next;
+    }
+    return (sum);
+}
