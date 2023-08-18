a8dfcd1dc7436234432e94a7b0adcce0191bc3a3
ZEYAD-8 <zeyadosama15@gmail.com>
2023-08-18 13:09:07 +0300

    Task 6: Sum list

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -1,19 +1,23 @@
 #include "lists.h"
-
+/**
+ * sum_dlistint - sums all the numbers in each node in the list
+ * @head: head of the list
+ * Return: the sum as integer.
+*/
 int sum_dlistint(dlistint_t *head)
 {
-    dlistint_t *tmp;
-    int sum = 0;
+	dlistint_t *tmp;
+	int sum = 0;
 
-    if (head == NULL)
-    {
-        return (0);
-    }
-    tmp = head;
-    while (tmp != NULL)
-    {
-        sum += tmp->n;
-        tmp = tmp->next;
-    }
-    return (sum);
+	if (head == NULL)
+	{
+		return (0);
+	}
+	tmp = head;
+	while (tmp != NULL)
+	{
+		sum += tmp->n;
+		tmp = tmp->next;
+	}
+	return (sum);
 }
