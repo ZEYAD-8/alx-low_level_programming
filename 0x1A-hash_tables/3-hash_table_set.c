fecd02ab93bd51cf5ecef5614a6184662a0e70ae
ZEYAD-8 <zeyadosama15@gmail.com>
2023-09-09 04:59:09 +0300

    Task 3: >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -6,75 +6,31 @@
  * @value: the node's vlaue
  *
  * Return: 1 if success, 0 otherwise
-int hash_table_set(hash_table_t *ht, const char *key, const char *value)
-{
-	unsigned long int index;
-	hash_node_t *new_node;
-	char *dup_value = NULL;
-	char *dup_key = NULL;
-
-	if (key == NULL)
-		return (0);
-	dup_key = malloc(strlen(key) + 1);
-	dup_key = strcpy(dup_key, key);
-	if (dup_key == NULL)
-		return (0);
-
-	if (value != NULL)
-	{
-		dup_value = strdup(value);
-		if (dup_value == NULL)
-		{
-			free (dup_key);
-			return (0);
-		}
-	}
-	index = key_index((unsigned char *)dup_key, ht->size);
-
-	new_node = malloc(sizeof(hash_node_t));
-
-	if (new_node == NULL)
-		return (0);
-
-	new_node->key = dup_key;
-	new_node->value = dup_value;
-	new_node->next = NULL;
-
-	if (ht->array == NULL)
-		return (0);
-
-	if ((ht->array)[index] == NULL)
-	{
-		(ht->array)[index] = new_node;
-	}
-	else
-	{
-		new_node->next = (ht->array)[index];
-		(ht->array)[index] = new_node;
-	}
-
-	return (1);
-}
 */
+
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *new_node;
-	char *dup_value = NULL;
-	char *dup_key = NULL;
+	hash_node_t *new_node = NULL;
+	unsigned long int index = 0;
+	char *cpy_v = NULL;
+	char *cpy_k = NULL;
+
+	if (ht == NULL || key == NULL)
+		return (0);
+
 	new_node = malloc(sizeof(new_node));
 	if (new_node == NULL)
 		return (0);
-	dup_key = (char*) malloc(strlen(key) + 1);
-	strcpy(dup_key, key);
+	cpy_k = (char *)malloc(strlen(key) + 1);
+	strcpy(cpy_k, key);
 	if (value != NULL)
 	{
-		dup_value =  (char*) malloc(strlen(value) + 1);
-		strcpy(dup_value, value);
+		cpy_v = (char *)malloc(strlen(value) + 1);
+		strcpy(cpy_v, value);
 	}
-	new_node->key = dup_key;
-	new_node->value = dup_value;
-	index = key_index((const unsigned char *)dup_key, ht->size);
+	new_node->key = cpy_k;
+	new_node->value = cpy_v;
+	index = key_index((unsigned char *)cpy_k, ht->size);
 
 	if (ht->array == NULL)
 		return (0);
