43777e60800523e59471591243f9a30071ae9573
ZEYAD-8 <zeyadosama15@gmail.com>
2023-09-09 05:16:51 +0300

    Task 3: >>> ht['betty'] = 'cool'

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -13,14 +13,16 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	char *dup_value = NULL;
 	char *dup_key = NULL;
-
 	if (key == NULL)
+	{
 		return (0);
+	}
 	dup_key = malloc(strlen(key) + 1);
 	dup_key = strcpy(dup_key, key);
 	if (dup_key == NULL)
+	{
 		return (0);
-
+	}
 	if (value != NULL)
 	{
 		dup_value = strdup(value);
@@ -31,19 +33,18 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		}
 	}
 	index = key_index((unsigned char *)dup_key, ht->size);
-
 	new_node = malloc(sizeof(hash_node_t));
-
 	if (new_node == NULL)
+	{
 		return (0);
-
+	}
 	new_node->key = dup_key;
 	new_node->value = dup_value;
 	new_node->next = NULL;
-
 	if (ht->array == NULL)
+	{
 		return (0);
-
+	}
 	if ((ht->array)[index] == NULL)
 	{
 		(ht->array)[index] = new_node;
@@ -53,6 +54,5 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		new_node->next = (ht->array)[index];
 		(ht->array)[index] = new_node;
 	}
-
 	return (1);
 }
